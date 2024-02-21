3ab8bc28947c16b7caac832beb56e9f6327c5669
Lucas de Luccas <lucas.luccas@sou.inteli.edu.br>
2024-02-21 17:17:03 -0300

    Correção do Murilo

--- codigo/dobot.py
+++ codigo/dobot.py
@@ -4,14 +4,14 @@ import pydobot
 
 available_ports = list_ports.comports()
 print(f'available ports: {[x.device for x in available_ports]}')
-port = available_ports[1].device
+port = available_ports[0].device
 
-device = pydobot.Dobot(port=port, verbose=True)
+device = pydobot.Dobot(port=port, verbose=False)
 
 (x, y, z, r, j1, j2, j3, j4) = device.pose()
 print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
-
-device.move_to(x + 20, y + 80, z, r, wait=False)
+device.speed(150, 150)
+device.move_to(x + 20, y + 80, z, r, wait=True)
 device.move_to(x + 20, y + 80, z - 80, r, wait=True)
 device.move_to(x + 20, y + 80, z, r, wait=True) 
 device.move_to(x, y, z, r, wait=True)  
