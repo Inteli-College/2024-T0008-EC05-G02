1f1b115488693794f9f8396f0484cb214153a2b9
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2024-02-21 09:53:27 -0300

    setup pagina controle do robô react

---
+++ codigo/interface-robo/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Control from './pages/control/control.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Control />
+  </React.StrictMode>,
+)
---
+++ codigo/interface-robo/src/pages/control/control.tsx
@@ -0,0 +1,8 @@
+function Control(){
+    return (
+        <div className={"maintext"}>
+            Hello from Control asidjashdiasjh
+        </div>
+    )
+}
+export default Control
