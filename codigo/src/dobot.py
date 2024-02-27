abbaf06981dd4947666fcd342eaa49e38ab64726
josevalencar <jose.silva@sou.inteli.edu.br>
2024-02-27 17:57:32 -0300

    feat(dobot): funções de movimentação

--- codigo/src/dobot.py
+++ codigo/src/dobot.py
@@ -5,6 +5,7 @@ from serial.tools import list_ports
 import inquirer
 import pydobot
 from yaspin import yaspin
+import time
 
 # Traz o spinner para apresentar uma animação enquanto o robô está se movendo
 spinner = yaspin(text="Processando...", color="yellow")
@@ -18,17 +19,11 @@ porta_escolhida = inquirer.prompt([
     inquirer.List("porta", message="Escolha a porta serial", choices=[x.device for x in available_ports])
 ])["porta"]
 
-# EDITAR PROMPT ABAIXO
-# perguntas = inquirer.prompt([
-#         inquirer.List("operacao", message="Qual operação deseja realizar?", choices=["Voltar para origem", "subtração","multiplicacao","divisao"]),
-#         inquirer.Text("a", message="Digite o primeiro número"),
-#         inquirer.Text("b", message="Digite o segundo número")
-#     ])
 
 # Cria uma instância do robô
 robo = pydobot.Dobot(port=porta_escolhida, verbose=False)
 
-robo.speed(300, 300)
+robo.speed(100, 100)
 
 (x, y, z, r, j1, j2, j3, j4) = robo.pose()
 print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
@@ -39,30 +34,40 @@ def origem_global():
 def origem_local(): 
     robo.move_to(x, y, z, r, wait=True) 
 
-def move_robo(xaxis): # adicionar parametros com valor padrão
-    robo.move_to(xaxis, y, z, r, wait=True) 
+def move_robo_x(xaxis=200): #LIMITE = 360
+    robo.move_to(x + xaxis, y, z, r, wait=True) 
 
-# robo.move_to(x + 40, y, z, r, wait=False)
-# robo.move_to(x, y, z, r, wait=True)  # we wait until this movement is done before continuing
+def move_robo_y(yaxis=100):
+    robo.move_to(x, y + yaxis, z, r, wait=True) 
 
-origem_global()
+def move_robo_z(zaxis=100):
+    robo.move_to(x, y, z + zaxis, r, wait=True) 
+
+def move_robo_r(raxis=100):
+    robo.move_to(x, y, z, r + raxis, wait=True) 
+
+def move_robo_location(xaxis, yaxis, zaxis):
+    robo.move_to(xaxis, yaxis, zaxis, r, wait=True) 
+
+def robo_speed(value):
+    robo.speed(value, value)
+    
+def pegar_remedio_vermelho():
+    robo.suck(False)
+    move_robo_location(282, 70, -4)
+    robo.suck(True)
+    time.sleep(5)
+    move_robo_z(30)
+    # origem_global()
+    move_robo_location(75, -212, 30)
+    robo.suck(False)
 
 
-# # Inicializa o efetuador do robô
-# spinner.start()
-# robo.suck(True)
-# # Adiciona um delay para o robô efetuar a operação
-# print("atuador acionado")
-# robo.wait(200)
-# spinner.stop()
+# origem_global()
+pegar_remedio_vermelho()
+# origem_global()
 
-# # Desliga o efetuador do robô
-# spinner.start()
-# robo.suck(False)
-# # Adiciona um delay para o robô efetuar a operação
-# print("atuador desligado")
-# robo.wait(200)
-# spinner.stop()
+# robo.move_to_J(131.24, 212.6, 151.72, 58.31)
 
 # Pega a posição atual do robô
 posicao_atual = robo.pose()
