cb622fdf05f80897a37c7599a01e1a559c9eed69
josevalencar <jose.silva@sou.inteli.edu.br>
2024-02-29 16:13:36 -0300

    feat integração da cli com automação

---
+++ codigo/src/CLI/classes/robo.py
@@ -0,0 +1,62 @@
+# Traz a ferramenta serial para apresentar quais portas estão disponíveis
+from serial.tools import list_ports
+import inquirer
+import pydobot
+from yaspin import yaspin
+import time
+
+
+class Robo():
+    def __init__(self, continuacao: callable, robo) -> None:
+        self.continuacao = continuacao
+        self.robo = robo
+        (self.x, self.y, self.z, self.r, self.j1, self.j2, self.j3, self.j4) = self.robo.pose()
+        print(f'x:{self.x} y:{self.y} z:{self.z} j1:{self.j1} j2:{self.j2} j3:{self.j3} j4:{self.j4}')
+
+    def origem_global(self): 
+        self.robo.move_to(237, -23, 149, self.r, wait=True) 
+
+    def origem_local(self): 
+        self.robo.move_to(self.x, self.y, self.z, self.r, wait=True) 
+
+    def move_robo_x(self, xaxis=200): #LIMITE = 360
+        self.robo.move_to(self.x + xaxis, self.y, self.z, self.r, wait=True) 
+
+    def move_robo_y(self,yaxis=100):
+        self.robo.move_to(self.x, self.y + yaxis, self.z, self.r, wait=True) 
+
+    def move_robo_z(self,zaxis=100):
+        self.robo.move_to(self.x, self.y, self.z + zaxis, self.r, wait=True) 
+
+    def move_robo_r(self, raxis=100):
+        self.robo.move_to(self.x, self.y, self.z, self.r + raxis, wait=True) 
+
+    def move_robo_location(self, xaxis, yaxis, zaxis):
+        self.robo.move_to(xaxis, yaxis, zaxis, self.r, wait=True) 
+
+    def change_speed(self, value):
+        self.robo.speed(value, value)
+
+    def close_connection(self):
+        self.robo.close()
+    
+    def ativar_ventosa(self, resposta):
+        if resposta == "a":
+            self.robo.suck(True)
+        if resposta == "d":
+            self.robo.suck(False)
+
+    def posicao_atual(self):
+        return print(f' Posição atual: x:{self.x} y:{self.y} z:{self.z} j1:{self.j1} j2:{self.j2} j3:{self.j3} j4:{self.j4}')
+    
+    def pegar_medicamento(self):
+        self.robo.move_to(101.83129119873047, -263.7802734375, 30.708972930908203, self.r, wait=True)
+        self.robo.move_to(101.83129119873047, -263.7802734375, -10.708972930908203, self.r, wait=True)
+        # self.robo.suck(True)
+        # self.robo.move_to(101.83129119873047, -263.7802734375, 60.708972930908203, self.r, wait=True)
+
+
+
+
+# # Fecha a conexão com o robô
+# robo.close()
--- codigo/src/CLI/main.py
+++ codigo/src/CLI/main.py
@@ -2,32 +2,51 @@ import typer
 import inquirer
 from yaspin import yaspin
 import time
+import pydobot
+from serial.tools import list_ports
 from classes.config import Configurar
 from classes.move import Movimentar
+from classes.robo import Robo
 # Cria uma instância da aplicação
+
 app = typer.Typer()
 
 # Estado da ferramenta
 robot_tool = False
 
+available_ports = list_ports.comports()
+
+porta_escolhida = inquirer.prompt([inquirer.List("porta", message="Escolha a porta serial", choices=[x.device for x in available_ports])])["porta"]
+
+print('Porta escolhida:', porta_escolhida)
+
+robo = pydobot.Dobot(port=porta_escolhida, verbose=False)
+
+(x, y, z, r, j1, j2, j3, j4) = robo.pose()
+print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
+
 # Cria um quarto comando do CLI
 @app.command()
 def inicio():
     # realiza lista de perguntas para o usuário
+
+    my_robot = Robo(continuacao=inicio, robo=robo)  # Pass both arguments to Robo
     
     perguntas = [inquirer.List("escolha", message="Deseja configurar o robô ou realizar movimentações?", choices=["configurações", "movimentações", "sair"])]
     # realiza a leitura das respostas
     respostas = inquirer.prompt(perguntas)
     match(respostas["escolha"]):
         case "configurações":
-            Modo = Configurar(inicio)
+            Modo = Configurar(inicio, my_robot)
             Modo.configurar()
         case "movimentações":
-            Modo = Movimentar(inicio)
+            Modo = Movimentar(inicio, my_robot)
             Modo.movimentar()
         case "sair":
-            print('Fechando')
+            robo.close()
+            print('Fechando...')
     # chama a funcao que processa a operação e exibe uma spinner para o usuário
+            
 # Executa a aplicação
 if __name__ == "__main__":
     app()
