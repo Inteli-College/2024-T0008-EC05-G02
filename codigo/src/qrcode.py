fde7d6bd62c35b75841de9b913ea5cb0230cbed9
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2024-03-11 16:21:15 -0300

    alteração do modelo de treinamento do qreader

--- codigo/src/qrcode.py
+++ codigo/src/qrcode.py
@@ -1,30 +1,29 @@
 import cv2
-from pyzbar import pyzbar
+from qreader import QReader
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 
 print("=" * 30)
-
 print("Procurando QR code...")
+
+qreader = QReader(model_size = 'l', min_confidence = 0.2)
 
 while True:
     _, image = camera.read()
 
-    barcodes = pyzbar.decode(image)
+    decoded_text = qreader.detect_and_decode(image=image)
 
-    if len(barcodes) > 0:
+    if decoded_text:
         break
 
 print("QR code encontrado!")
-cv2.imwrite("codigo/src/qrcodes/frame.png", image)
+cv2.imwrite("qrcodes/frame.png", image)
 
 print("Fechando a câmera...")
 camera.release()
 
 print("Imagem salva!")
-image = cv2.cvtColor(cv2.imread("frame.png"), cv2.COLOR_BGR2RGB)
-
-decoded_text = barcodes[0].data.decode("utf-8")
+image = cv2.cvtColor(cv2.imread("qrcodes/frame.png"), cv2.COLOR_BGR2RGB)
 
 print("Leitura feita:", decoded_text)
 print("=" * 30)
