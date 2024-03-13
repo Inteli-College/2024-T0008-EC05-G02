7c9281771264d31f6eaed37c0631e7b9f24b253f
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2024-03-13 16:56:28 -0300

    integracao leitura qrcode com validacao dos dados

--- codigo/src/backend/main.py
+++ codigo/src/backend/main.py
@@ -3,14 +3,18 @@ import cv2
 import threading
 import time
 from qreader import QReader
+import uvicorn
+from datetime import datetime
+import json
+from dateutil.relativedelta import relativedelta
 
 
-qreader = QReader()
+qreader = QReader(model_size = 'l', min_confidence = 0.3)
 last_qr_data = None
 
 def qr_code_detection():
     global last_qr_data
-    cap = cv2.VideoCapture(0)  # 0 é geralmente o ID da primeira webcam detectada
+    cap = cv2.VideoCapture(1)  # 0 é geralmente o ID da primeira webcam detectada
 
     # Supondo que qreader.detect_and_decode funcione diretamente com imagens no formato do OpenCV
     while True:
@@ -23,6 +27,7 @@ def qr_code_detection():
             if data:
                 last_qr_data = data[0]
                 print(f"QR Code detectado: {data}")
+
                 # break # Remova ou comente esta linha se deseja detecção contínua
 
         time.sleep(2)  # Pausa para não sobrecarregar o CPU
@@ -40,7 +45,7 @@ async def root():
 @app.get("/qreader/")
 async def read_qr_code():
     global last_qr_data
-    cap = cv2.VideoCapture(0)  # 0 é geralmente o ID da primeira webcam detectada
+    cap = cv2.VideoCapture(1)  # 0 é geralmente o ID da primeira webcam detectada
 
     # Supondo que qreader.detect_and_decode funcione diretamente com imagens no formato do OpenCV
     while True:
@@ -53,8 +58,26 @@ async def read_qr_code():
             if data:
                 last_qr_data = data[0]
                 print(f"QR Code detectado: {data}")
+                cv2.imwrite(f"backend/qrcodes/{data[0]}.jpg", rgb_img)
                 if data[0] is not None:
-                    break
+                    six_months_ahead = (datetime.now() + relativedelta(months=6)).strftime("%Y-%m-%d")
+                    try:
+                        drug_info = json.loads(data[0])
+                    except json.JSONDecodeError:
+                        return {"error": "Invalid QR code content"}, 400
+                    # Extract information into variables
+                    name = drug_info.get("Name")
+                    dose = drug_info.get("Dose")
+                    due_date = drug_info.get("Due date")
+                    lot = drug_info.get("Lot")
+                    supplier = drug_info.get("Supplier")
+
+                    if name != "Dobutamina + Glicose 50%" or due_date < six_months_ahead:
+                        return {"error": "medicamento próximo do vencimento ou não é o medicamento correto"}
+                    else:
+                        return {"name": name, "dose": dose, "due_date": due_date, "lot": lot, "supplier": supplier}
         print("Tentando ler QRCODE")
         time.sleep(2)  # Pausa para não sobrecarregar o CPU
-    return {"data": last_qr_data}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
