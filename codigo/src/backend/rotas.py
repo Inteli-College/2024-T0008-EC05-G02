51e02286856bf3ee52c50f4c6fc33ee2bcef22f1
Ana <ana.marques@sou.inteli.edu.br>
2024-03-14 15:16:46 -0300

    modificação de rotas

--- codigo/src/backend/rotas.py
+++ codigo/src/backend/rotas.py
@@ -9,26 +9,41 @@ cur = conn.cursor()
 app = FastAPI()
 
 class Item(BaseModel):
+    item_id : int
+    id_operacao: int
     nome: str
     lote: int
     validade: int
     imagem: str
 
+class Operacao(BaseModel):
+    id: int
+    responsavel: str
+    data: int
+    id_carrinho: int
+    tipo: int
+
 # Rota para obter todos os itens
 @app.get("/itens/")
 async def get_usuarios():
-    cur.execute("SELECT * FROM item")
+    cur.execute("SELECT * FROM itens")
     itens = cur.fetchall()
     return itens
 
-@app.get("/carrinhos/")
+@app.get("/operacoes/")
 async def get_usuarios():
-    cur.execute("SELECT * FROM carrinho")
+    cur.execute("SELECT * FROM operacoes")
     itens = cur.fetchall()
     return itens
 
-@app.post("/adicionar_itens/")
+@app.post("/adicionar_item/")
 async def criar_usuario(Item:Item):
-    cur.execute("INSERT INTO item (nome, lote, validade, imagem) VALUES (?, ?,?,?)", (Item.nome,Item.lote,Item.validade,Item.imagem))
+    cur.execute("INSERT INTO itens (item_id,nome, lote, validade, imagem, id_operacao) VALUES (?,?,?,?,?,?)", (Item.item_id,Item.nome,Item.lote,Item.validade,Item.imagem, Item.id_operacao))
+    conn.commit()
+    return {"status": "Usuário criado com sucesso"}
+
+@app.post("/adicionar_operacao/")
+async def criar_usuario(Operacao: Operacao):
+    cur.execute("INSERT INTO operacao (id, responsavel, data, id_carrinho, tipo) VALUES (?,?,?,?,?)", (Operacao.id,Operacao.data,Operacao.responsavel,Operacao.id_carrinho, Operacao.tipo))
     conn.commit()
     return {"status": "Usuário criado com sucesso"}
