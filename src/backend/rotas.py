d75ceaafabbdce807619fdd9df7defcb5c978684
GuilhermeLinhares04 <guilherme.linhares@sou.inteli.edu.br>
2024-03-21 17:54:29 -0300

    Correção e atualização de rotas

--- src/backend/rotas.py
+++ src/backend/rotas.py
@@ -47,6 +47,7 @@ async def get_usuarios():
     carrinhos = cur.fetchall()
     return carrinhos
 
+# Rotas para alterar os itens
 @app.post("/adicionar_bipagem/")
 async def criar_usuario(Bipagem: Bipagem):
     cur.execute("INSERT INTO Bipagem (id_item,nome, lote, validade, fornecedor, id_operacao) VALUES (?,?,?,?,?,?)", (Bipagem.id_item,Bipagem.nome,Bipagem.lote,Bipagem.validade,Bipagem.fornecedor, Bipagem.id_operacao))
@@ -65,26 +66,37 @@ async def criar_usuario(Carrinho: Carrinho):
     conn.commit()
     return {"status": "Carrinho adicionado com sucesso"}
 
-# Deleção do carrinho (19/03)
-@app.delete("/deletar_carrinho/{id}")
-async def deletar_usuario(id: int):
-    cur.execute("DELETE FROM carrinhos WHERE id=?", (id,))
+# Novas rotas (19/03)
+@app.delete("/deletar_carrinho/")
+async def deletar_usuario(Carrinho: Carrinho):
+    cur.execute("DELETE FROM carrinhos WHERE id=?", (Carrinho.id,))
     conn.commit()
     return {"status": "Carrinho deletado com sucesso"}
 
-# Atualização de carrinho (19/03)
 @app.post("/atualizar_carrinho/")
 async def atualizar_usuario(Carrinho: Carrinho):
     cur.execute("UPDATE carrinhos SET car_layout=? WHERE id=?", (Carrinho.car_layout, Carrinho.id))
     conn.commit()
     return {"status": "Carrinho atualizado com sucesso"}
 
-# Atualização da bipagem no reabastecimento (19/03)
 @app.post("/atualizar_bipagem/")
 async def atualizar_usuario(Bipagem: Bipagem):
     cur.execute("UPDATE Bipagem SET id_item=?,nome=?,lote=?,validade=?,fornecedor=? WHERE id_operacao=?", (Bipagem.id_item,Bipagem.nome,Bipagem.lote,Bipagem.validade,Bipagem.fornecedor, Bipagem.id_operacao))
     conn.commit()
     return {"status": "Bipagem atualizada com sucesso"}
+
+@app.post("/atualizar_operacao/")
+async def atualizar_usuario(Operacao: Operacao):
+    cur.execute("UPDATE operacoes SET id_responsavel=?,id_carrinho=?,data=?,tipo=? WHERE id=?", (Operacao.id_responsavel,Operacao.id_carrinho,Operacao.data,Operacao.tipo, Operacao.id))
+    conn.commit()
+    return {"status": "Operação atualizada com sucesso"}
+
+# Rota indireta de teste
+@app.delete("/deletar_operacao/")
+async def deletar_usuario(Operacao: Operacao):
+    cur.execute("DELETE FROM operacoes WHERE id=?", (Operacao.id,))
+    conn.commit()
+    return {"status": "Operação deletada com sucesso"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
