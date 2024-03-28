85d1a919dd45bdffe6dbdd82d61b5b7a46beb630
GuilhermeLinhares04 <guilherme.linhares@sou.inteli.edu.br>
2024-03-28 14:15:30 -0300

    Atualização manual

--- src/frontend/src/pages/manual/index.js
+++ src/frontend/src/pages/manual/index.js
@@ -75,25 +75,25 @@ function ManualInstrucoes() {
           <div className="header" id="header1">
             <h2>Introdução</h2>
             <p>
-              
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ipsum vitae massa euismod vehicula. Donec interdum, turpis sed mattis dignissim, dui ligula maximus lacus, vulputate egestas est orci at dolor. Nulla et massa ex. Fusce at magna ut nisi dapibus cursus. Morbi ultrices viverra turpis, a vestibulum felis hendrerit et. Sed urna velit, pulvinar at viverra nec, interdum vel nunc. Quisque quis ligula a ante mattis blandit. Duis sagittis elit at condimentum aliquam. Curabitur sollicitudin purus tortor. Aenean tempus, libero vitae congue lacinia, enim elit feugiat massa, eget gravida urna turpis ac nunc. Proin ex libero, ultrices sed elementum sit amet, euismod id urna. Integer congue vestibulum tellus eu vehicula. Nulla et magna porttitor, sodales metus in, accumsan arcu. Nunc in lorem in ipsum tempus posuere. Nulla in quam dignissim, posuere turpis et, pulvinar orci. Etiam scelerisque sit amet sapien et tincidunt.
             </p>
           </div>
           <div className="header" id="header2">
             <h2>Criar</h2>
             <p>
-              
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ipsum vitae massa euismod vehicula. Donec interdum, turpis sed mattis dignissim, dui ligula maximus lacus, vulputate egestas est orci at dolor. Nulla et massa ex. Fusce at magna ut nisi dapibus cursus. Morbi ultrices viverra turpis, a vestibulum felis hendrerit et. Sed urna velit, pulvinar at viverra nec, interdum vel nunc. Quisque quis ligula a ante mattis blandit. Duis sagittis elit at condimentum aliquam. Curabitur sollicitudin purus tortor. Aenean tempus, libero vitae congue lacinia, enim elit feugiat massa, eget gravida urna turpis ac nunc. Proin ex libero, ultrices sed elementum sit amet, euismod id urna. Integer congue vestibulum tellus eu vehicula. Nulla et magna porttitor, sodales metus in, accumsan arcu. Nunc in lorem in ipsum tempus posuere. Nulla in quam dignissim, posuere turpis et, pulvinar orci. Etiam scelerisque sit amet sapien et tincidunt.
             </p>
           </div>
           <div className="header" id="header3">
             <h2>Abastecer</h2>
             <p>
-              
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ipsum vitae massa euismod vehicula. Donec interdum, turpis sed mattis dignissim, dui ligula maximus lacus, vulputate egestas est orci at dolor. Nulla et massa ex. Fusce at magna ut nisi dapibus cursus. Morbi ultrices viverra turpis, a vestibulum felis hendrerit et. Sed urna velit, pulvinar at viverra nec, interdum vel nunc. Quisque quis ligula a ante mattis blandit. Duis sagittis elit at condimentum aliquam. Curabitur sollicitudin purus tortor. Aenean tempus, libero vitae congue lacinia, enim elit feugiat massa, eget gravida urna turpis ac nunc. Proin ex libero, ultrices sed elementum sit amet, euismod id urna. Integer congue vestibulum tellus eu vehicula. Nulla et magna porttitor, sodales metus in, accumsan arcu. Nunc in lorem in ipsum tempus posuere. Nulla in quam dignissim, posuere turpis et, pulvinar orci. Etiam scelerisque sit amet sapien et tincidunt.
             </p>
           </div>
           <div className="header" id="header4">
             <h2>Itens Faltantes</h2>
             <p>
-            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a ipsum vitae massa euismod vehicula. Donec interdum, turpis sed mattis dignissim, dui ligula maximus lacus, vulputate egestas est orci at dolor. Nulla et massa ex. Fusce at magna ut nisi dapibus cursus. Morbi ultrices viverra turpis, a vestibulum felis hendrerit et. Sed urna velit, pulvinar at viverra nec, interdum vel nunc. Quisque quis ligula a ante mattis blandit. Duis sagittis elit at condimentum aliquam. Curabitur sollicitudin purus tortor. Aenean tempus, libero vitae congue lacinia, enim elit feugiat massa, eget gravida urna turpis ac nunc. Proin ex libero, ultrices sed elementum sit amet, euismod id urna. Integer congue vestibulum tellus eu vehicula. Nulla et magna porttitor, sodales metus in, accumsan arcu. Nunc in lorem in ipsum tempus posuere. Nulla in quam dignissim, posuere turpis et, pulvinar orci. Etiam scelerisque sit amet sapien et tincidunt.
             </p>
         </div>
       </div>
