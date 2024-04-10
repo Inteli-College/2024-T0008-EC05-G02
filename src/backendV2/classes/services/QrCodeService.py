40c93472fc35cee85998a0e2634d6bfb6a2d2c7b
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2024-04-10 15:16:59 -0300

    COMMIT RAPIDO PRO ZE CONSEGUIR RODA RNO PC DELE EU JOGO LOL

--- src/backendV2/classes/services/QrCodeService.py
+++ src/backendV2/classes/services/QrCodeService.py
@@ -14,17 +14,6 @@ class QRCodeService:
         self.cam = cv2.VideoCapture(0)
         print("Camera initialized")
         self.qr = QReader(model_size='l', min_confidence=0.3)
-    async def read_qr_code2(self):
-        while True:
-            ret, img = self.cam.read()
-            if ret:
-                rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                data = self.qr.detect_and_decode(rgb_img)
-                if data and data[0] is not None:
-                    return(f"QR Code detected: {data}")
-                
-                    break
-            await asyncio.sleep(2)  # non-blocking sleep
 
 
     async def read_qr_code(self,expected_medication: dict):
@@ -42,7 +31,7 @@ class QRCodeService:
         print(f"QR Code data: {qr_data}")
 
         if qr_data['Nome'] != expected_medication['Nome']:
-            return 'Medicamento incorreto', qr_data
+            return 'Medicamento incorreto', [qr_data['Nome'],qr_data['Dose'],qr_data['Data de validade'],qr_data['Lote'],qr_data['Fornecedor']]
         expiration_date_str = qr_data.get("Data de validade")
 
         if qr_data.get('Dosagem') != expected_medication.get('Dosagem'):
@@ -52,7 +41,7 @@ class QRCodeService:
             expiration_date = datetime.strptime(expiration_date_str, "%d/%m/%Y")
         except ValueError as e:
             print(f"Error parsing date: {e}")
-            return 'Erro de formatação da data de validade', qr_data
+            return 'Erro de formatação da data de validade', [qr_data['Nome'],qr_data['Dose'],qr_data['Data de validade'],qr_data['Lote'],qr_data['Fornecedor']]
 
         current_date = datetime.now()
         six_months_ahead = current_date + timedelta(days=6*30)
@@ -60,13 +49,13 @@ class QRCodeService:
         # Compare the expiration date to the current date
         if expiration_date < current_date:
             # Medication is expired
-            return 'Medicamento correto, mas expirado', qr_data
+            return 'Medicamento correto, mas expirado', [qr_data['Nome'],qr_data['Dose'],qr_data['Data de validade'],qr_data['Lote'],qr_data['Fornecedor']]
         elif expiration_date < six_months_ahead:
             # Medication is close to expiration
-            return 'Medicamento correto, mas próximo da data de validade', qr_data
+            return 'Medicamento correto, mas próximo da data de validade', [qr_data['Nome'],qr_data['Dose'],qr_data['Data de validade'],qr_data['Lote'],qr_data['Fornecedor']]
         else:
             # Medication is valid
-            return 'Medicamento correto e válido', qr_data
+            return 'Medicamento correto e válido', [qr_data['Nome'],qr_data['Dose'],qr_data['Data de validade'],qr_data['Lote'],qr_data['Fornecedor']]
 
         
 
